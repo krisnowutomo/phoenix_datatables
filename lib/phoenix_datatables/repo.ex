4fe309ed5922d0f83848203692be0e42a382cb1a
krisnowutomo <41182167+krisnowutomo@users.noreply.github.com>
2025-06-23 15:46:53 +0700

    add function to catch repo adapter

--- lib/phoenix_datatables/repo.ex
+++ lib/phoenix_datatables/repo.ex
@@ -15,6 +15,24 @@ defmodule PhoenixDatatables.Repo do
       def fetch_datatable(query, params, options \\ nil) do
         PhoenixDatatables.execute(query, params, __MODULE__, options)
       end
+
+      @doc false # Sembunyikan dari dokumentasi publik jika tidak perlu
+      # Fungsi helper untuk mendapatkan adapter repo yang sedang digunakan
+      # __CALLER__.module akan berisi modul Repo yang memanggil `use` (misalnya MyApp.Repo)
+      def get_repo_adapter do
+        # Pastikan modul repo yang memanggil ini memiliki fungsi :adapter, 0
+        caller_repo_module = __CALLER__.module
+
+        if function_exported?(caller_repo_module, :adapter, 0) do
+          caller_repo_module.adapter()
+        else
+          raise """
+          PhoenixDatatables Error: The module `#{inspect caller_repo_module}`
+          that uses `PhoenixDatatables.Repo` does not appear to be a valid Ecto Repo.
+          It must have an `adapter/0` function.
+          """
+        end
+      end
     end
   end
 
